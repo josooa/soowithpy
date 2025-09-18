545edd947442e3f1c2233499098f16229627dd44
josooa <jsua68652@gmail.com>
2025-09-18 11:01:00 +0900

    class_score_analysis

---
+++ class_score_analysis/class_score_analysis.py
@@ -0,0 +1,57 @@
+from fileinput import filename
+
+
+def read_data(filename):
+    # TODO) Read `filename` as a list of integers
+    data = []
+    f = open(filename, 'r')
+    lines = f.readlines()[1:] # Skip the header line
+    for line in lines:
+        items = line.strip().split(',')
+        if len(items) == 2:
+             data.append((int(items[0]), int(items[1])))
+    
+    return data
+
+def calc_weighted_average(data_2d, weight):
+    # TODO) Calculate the weighted averages of each row of `data_2d`
+    average = []
+    for row in data_2d:
+        average.append(float(row[0]) * weight[0] + float(row[1]) * weight[1])
+    return average
+
+def analyze_data(data_1d):
+    # TODO) Calculate summary statistics of the given `data_1d`
+    # Note) Please don't use NumPy and other libraries. Do it yourself.
+    data_1d = sorted(data_1d)
+    mean = sum(data_1d) / len(data_1d) if data_1d else 0
+    var = sum((x - mean) ** 2 for x in data_1d) / len(data_1d) if data_1d else 0
+    median = data_1d[len(data_1d) // 2] if data_1d else 0
+    return mean, var, median, min(data_1d), max(data_1d)
+
+if __name__ == '__main__':
+    data = read_data('C:\\Users\\josoo\\soowithpy\\class_score_analysis\\data\\class_score_en.csv')
+    if data and len(data[0]) == 2: # Check 'data' is valid
+        average = calc_weighted_average(data, [40/125, 60/100])
+
+        # Write the analysis report as a markdown file
+        with open('C:\\Users\\josoo\\soowithpy\\class_score_analysis\\class_score_analysis.md', 'w') as report:
+            report.write('### Individual Score\n\n')
+            report.write('| Midterm | Final | Average |\n')
+            report.write('| ------- | ----- | ----- |\n')
+            for ((m_score, f_score), a_score) in zip(data, average):
+                report.write(f'| {m_score} | {f_score} | {a_score:.3f} |\n')
+            report.write('\n\n\n')
+
+            report.write('### Examination Analysis\n')
+            data_columns = {
+                'Midterm': [m_score for m_score, _ in data],
+                'Final'  : [f_score for _, f_score in data],
+                'Average': average }
+            for name, column in data_columns.items():
+                mean, var, median, min_, max_ = analyze_data(column)
+                report.write(f'* {name}\n')
+                report.write(f'  * Mean: **{mean:.3f}**\n')
+                report.write(f'  * Variance: {var:.3f}\n')
+                report.write(f'  * Median: **{median:.3f}**\n')
+                report.write(f'  * Min/Max: ({min_:.3f}, {max_:.3f})\n')
--- covid19_statistics/covid19_statistics.md
+++ covid19_statistics/covid19_statistics.md
@@ -24,22 +24,22 @@
 ### Korean COVID-19 New Cases by Region
 * Total new cases: 1714
 
-| Region | Population | Ratio (%) | New Cases per 1M |
+| Region | New Cases | Ratio (%) | New Cases per 1M |
 | ------ | ---------- | --------- | ----------------- |
-| Seoul | 9550227 | 37.6 | 67.4 |
-| Gyeongi | 13530519 | 30.9 | 39.1 |
-| Busan | 3359527 | 2.2 | 11.3 |
-| Gyeongnam | 3322373 | 1.7 | 8.7 |
-| Incheon | 2938429 | 8.6 | 50.4 |
-| Gyeongbuk | 2630254 | 1.6 | 10.6 |
-| Daegu | 2393626 | 2.4 | 17.1 |
-| Chungnam | 2118183 | 3.6 | 29.3 |
-| Jeonnam | 1838353 | 1.3 | 12.5 |
-| Jeonbuk | 1792476 | 1.6 | 15.1 |
-| Chungbuk | 1597179 | 1.6 | 16.9 |
-| Gangwon | 1536270 | 1.9 | 21.5 |
-| Daejeon | 1454679 | 0.9 | 11.0 |
-| Gwangju | 1441970 | 2.3 | 27.7 |
-| Ulsan | 1124459 | 1.2 | 17.8 |
-| Jeju | 675883 | 0.3 | 7.4 |
-| Sejong | 365309 | 0.2 | 10.9 |
+| Seoul | 644 | 37.6 | 67.4 |
+| Gyeongi | 529 | 30.9 | 39.1 |
+| Busan | 38 | 2.2 | 11.3 |
+| Gyeongnam | 29 | 1.7 | 8.7 |
+| Incheon | 148 | 8.6 | 50.4 |
+| Gyeongbuk | 28 | 1.6 | 10.6 |
+| Daegu | 41 | 2.4 | 17.1 |
+| Chungnam | 62 | 3.6 | 29.3 |
+| Jeonnam | 23 | 1.3 | 12.5 |
+| Jeonbuk | 27 | 1.6 | 15.1 |
+| Chungbuk | 27 | 1.6 | 16.9 |
+| Gangwon | 33 | 1.9 | 21.5 |
+| Daejeon | 16 | 0.9 | 11.0 |
+| Gwangju | 40 | 2.3 | 27.7 |
+| Ulsan | 20 | 1.2 | 17.8 |
+| Jeju | 5 | 0.3 | 7.4 |
+| Sejong | 4 | 0.2 | 10.9 |
